9683b35fcc41b0485a6a4da9121d82ccd981d2d9
KIKETENATHAN <kiketenathan@gmail.com>
2025-06-23 17:17:53 +0300

    header section rectified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -106,7 +106,9 @@
                             <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
                                 <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
                             </svg>
-                            Empowering TVET Institutions in Kenya
+                            <h1 class="text-xl font-bold bg-yellow-400 text-blue-900 px-4 py-1 rounded">
+                                Empowering TVET Institutions in Kenya
+                            </h1>
                         </div>
                         <h1 class="text-4xl lg:text-6xl font-bold text-gray-900 leading-tight text-shadow">
                             Go <span class="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-indigo-600">Paperless</span> with Confidence
